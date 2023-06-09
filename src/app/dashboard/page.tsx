6630cfa350c655e60e80be38a91ba16b824d3747
ahmed nasser <ahmednas197@gmail.com>
2023-06-09 22:42:35 +0300

    integrate the database

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 
-interface DashboardProps {}
+interface DashboardProps { }
 
 const Dashboard: FC<DashboardProps> = () => {
   return <div>Dashboard</div>;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import Button from "../ui/Button";
+import { db } from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+
   return (
     <div className="text-red-400">
       Test
