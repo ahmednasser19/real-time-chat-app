d1413ad529df36147ef77385175b125348af8783
ahmed nasser <ahmednas197@gmail.com>
2023-06-09 22:33:46 +0300

    add project setup and initialize components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import Button from "../ui/Button";
 
 export default function Home() {
-  return <div className="text-red-400">Test</div>;
+  return (
+    <div className="text-red-400">
+      Test
+      <Button>Test</Button>
+    </div>
+  );
 }
