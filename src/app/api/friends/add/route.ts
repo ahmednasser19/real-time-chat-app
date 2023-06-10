56c33d0513006c835e4e5e4870051056fc75ee4a
ahmed nasser <ahmednas197@gmail.com>
2023-06-10 15:23:18 +0300

    create database helper function and finishing add friend feature

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -1,30 +1,36 @@
+import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
 import { addFriendValidator } from "@/lib/validations/add-freind";
 import { getServerSession } from "next-auth";
+import { z } from "zod";
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
     const { email: emailToAdd } = addFriendValidator.parse(body.email);
 
-    const RESTResponse = await fetch(
-      `${process.env.UPSTASH_REDIS_REST_URL}/get/user:email${emailToAdd}`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`,
-        },
-        cache: "no-store",
-      }
-    );
+    // const RESTResponse = await fetch(
+    //   `${process.env.UPSTASH_REDIS_REST_URL}/get/user:email:${emailToAdd}`,
+    //   {
+    //     headers: {
+    //       Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`,
+    //     },
+    //     cache: "no-store",
+    //   }
+    // );
 
-    const data = (await RESTResponse.json()) as { result: string | null };
+    // const data = (await RESTResponse.json()) as { result: string | null };
 
-    const idToAdd = data.result;
+    const idToAdd = (await fetchRedis(
+      "get",
+      `user:email:${emailToAdd}`
+    )) as string;
 
     const session = await getServerSession(authOptions);
 
     if (!idToAdd) {
-      return new Response("User not found", { status: 400 });
+      return new Response("This person does not exist.", { status: 400 });
     }
 
     if (!session) {
@@ -35,6 +41,37 @@ export async function POST(req: Request) {
       return new Response("You can't add yourself", { status: 400 });
     }
 
-    console.log("data", data);
-  } catch (error) {}
+    //check if user is already a friend
+    // check if user is already added
+    const isAlreadyAdded = (await fetchRedis(
+      "sismember",
+      `user:${idToAdd}:incoming_friend_requests`,
+      session.user.id
+    )) as 0 | 1;
+
+    if (isAlreadyAdded) {
+      return new Response("Already added this user", { status: 400 });
+    }
+
+    // check if user is already added
+    const isAlreadyFriends = (await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:friends`,
+      idToAdd
+    )) as 0 | 1;
+
+    if (isAlreadyFriends) {
+      return new Response("Already friends with this user", { status: 400 });
+    }
+
+    await db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id);
+
+    return new Response("Friend request sent", { status: 200 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid request payload", { status: 422 });
+    }
+
+    return new Response("Invalid request", { status: 400 });
+  }
 }
