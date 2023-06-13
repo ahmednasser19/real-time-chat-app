1c467a14336b06b00d874ff2c362050658750fef
ahmed nasser <ahmednas197@gmail.com>
2023-06-13 11:35:31 +0300

    Added real-time communication & logic for chat

--- src/app/api/message/send/route.ts
+++ src/app/api/message/send/route.ts
@@ -1,6 +1,8 @@
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { pusherServer } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 import { Message, messageValidator } from "@/lib/validations/message";
 import { nanoid } from "nanoid";
 import { getServerSession } from "next-auth";
@@ -51,6 +53,23 @@ export async function POST(req: Request) {
 
     const message = messageValidator.parse(messageData);
 
+    // notify all connected chat room clients
+    await pusherServer.trigger(
+      toPusherKey(`chat:${chatId}`),
+      "incoming-message",
+      message
+    );
+
+    await pusherServer.trigger(
+      toPusherKey(`user:${friendId}:chats`),
+      "new_message",
+      {
+        ...message,
+        senderImg: sender.image,
+        senderName: sender.name,
+      }
+    );
+
     await db.zadd(`chat:${chatId}:messages`, {
       score: timestamp,
       member: JSON.stringify(message),
@@ -59,11 +78,9 @@ export async function POST(req: Request) {
     return new Response("Ok");
   } catch (error) {
     if (error instanceof Error) {
-      return new Response(error.message, { status: 500 })
+      return new Response(error.message, { status: 500 });
     }
 
-    return new Response('Internal Server Error', { status: 500 })
+    return new Response("Internal Server Error", { status: 500 });
   }
 }
-  
-
